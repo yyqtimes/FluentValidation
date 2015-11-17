132e288c185e8ace9023635525fbf4cd638c0c1a
yyq <yyqtimes@163.com>
2015-11-17 14:22:53 +0800

    添加Class类

---
+++ MVFModel/ClassModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVFModel
+{
+    /// <summary>
+    /// 
+    /// Version 1.0
+    /// add by yyq at 2015/11/17 14:12:08
+    /// </summary>
+    public class ClassModel
+    {
+        public string ClassName { get; set; }
+    }
+}
